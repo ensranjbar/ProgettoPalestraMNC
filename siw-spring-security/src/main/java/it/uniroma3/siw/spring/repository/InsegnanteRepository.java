dbcfb9a1bd6e4bcdbf1b256406bfb5710f41ed7d
ensranjbar <55926968+ensranjbar@users.noreply.github.com>
2021-06-23 17:49:31 +0200

    repository insegnante -corso

---
+++ siw-spring-security/src/main/java/it/uniroma3/siw/spring/repository/InsegnanteRepository.java
@@ -0,0 +1,17 @@
+package it.uniroma3.siw.spring.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.spring.model.Insegnante;
+
+public interface InsegnanteRepository extends CrudRepository<Insegnante, Long> {
+
+	public List<Insegnante> findByNome(String nome);
+	public List<Insegnante> findByCognome(String cognome);
+	public List<Insegnante> findByNomeAndCognome(String nome,String Cognome);
+	public List<Insegnante> findByNomeOrCognome(String nome,String Cognome);
+
+	
+}
