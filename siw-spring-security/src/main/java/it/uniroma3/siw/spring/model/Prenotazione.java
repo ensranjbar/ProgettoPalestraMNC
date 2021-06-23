3ed7cbdf37e4d88d5b45954a5c86b0fa9b2f4898
matscultz <72978985+matscultz@users.noreply.github.com>
2021-06-23 11:49:25 +0200

    Classe Cliente e Prenotazione, aggiunte classi controller vuote

--- siw-spring-security/src/main/java/it/uniroma3/siw/spring/model/Prenotazione.java
+++ siw-spring-security/src/main/java/it/uniroma3/siw/spring/model/Prenotazione.java
@@ -1,5 +1,21 @@
 package it.uniroma3.siw.spring.model;
 
-public class Prenotazione {
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@NonNull
+public class Prenotazione {
+	private Long id;
+	@OneToOne
+	private Lezione lezione;
+	@ManyToOne
+	private Cliente cliente ;
 }
