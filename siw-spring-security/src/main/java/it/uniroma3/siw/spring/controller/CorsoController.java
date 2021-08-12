385decb7afeaaa27a4ae58d6920361971c7139b5
ensranjbar <55926968+ensranjbar@users.noreply.github.com>
2021-08-12 11:05:12 +0200

    controller,validator-->insegnante ,corso

--- siw-spring-security/src/main/java/it/uniroma3/siw/spring/controller/CorsoController.java
+++ siw-spring-security/src/main/java/it/uniroma3/siw/spring/controller/CorsoController.java
@@ -1,5 +1,59 @@
 package it.uniroma3.siw.spring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import it.uniroma3.siw.spring.controller.validator.CorsoValidator;
+
+import it.uniroma3.siw.spring.model.Corso;
+import it.uniroma3.siw.spring.service.CorsoService;
+
+@Controller
 public class CorsoController {
 
+	@Autowired
+	private CorsoService corsoService;
+
+	@Autowired
+	private CorsoValidator corsoValidator;
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@RequestMapping(value = "/addCorso", method = RequestMethod.GET)
+	public String addCorso(Model model) {
+		logger.debug("addCorso");
+		model.addAttribute("corso", new Corso());
+		return "corsoForm.html";
+	}
+
+	@RequestMapping(value = "/corso/{id}", method = RequestMethod.GET)
+	public String getCorso(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("corso", this.corsoService.corsoPerId(id));
+		return "corso.html";
+	}
+
+	@RequestMapping(value = "/corso", method = RequestMethod.GET)
+	public String getcorsi(Model model) {
+		model.addAttribute("corsi", this.corsoService.tutti());
+		return "corsi.html";
+	}
+
+	@RequestMapping(value = "/corso", method = RequestMethod.POST)
+	public String newCorso(@ModelAttribute("corso") Corso corso, Model model, BindingResult bindingResult) {
+		this.corsoValidator.validate(corso, bindingResult);
+		if (!bindingResult.hasErrors()) {
+			this.corsoService.inserisci(corso);
+			model.addAttribute("corsi", this.corsoService.tutti());
+			return "corsi.html";
+		}
+		return "corsoForm.html";
+	}
 }
--- siw-spring-security/src/main/java/it/uniroma3/siw/spring/controller/InsegnanteController.java
+++ siw-spring-security/src/main/java/it/uniroma3/siw/spring/controller/InsegnanteController.java
@@ -1,5 +1,60 @@
 package it.uniroma3.siw.spring.controller;
 
-public class InsegnanteController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import it.uniroma3.siw.spring.controller.validator.InsegnanteValidator;
+import it.uniroma3.siw.spring.model.Insegnante;
+import it.uniroma3.siw.spring.service.InsegnanteService;
+
+@Controller
+public class InsegnanteController {
+	
+	@Autowired
+	private InsegnanteService insegnanteService;
+	
+    @Autowired
+    private InsegnanteValidator insegnanteValidator;
+    
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+
+    @RequestMapping(value="/addInsegnante", method = RequestMethod.GET)
+    public String addInsegnante(Model model) {
+    	logger.debug("addInsegnante");
+    	model.addAttribute("insegnante", new Insegnante());
+        return "insegnanteForm.html";
+    }
+
+    @RequestMapping(value = "/insegnante/{id}", method = RequestMethod.GET)
+    public String getInsegnante(@PathVariable("id") Long id, Model model) {
+    	model.addAttribute("insegnante", this.insegnanteService.insegnantePerId(id));
+    	return "insegnante.html";
+    }
+
+    @RequestMapping(value = "/insegnante", method = RequestMethod.GET)
+    public String getinsegnanti(Model model) {
+    		model.addAttribute("insegnanti", this.insegnanteService.tutti());
+    		return "insegnanti.html";
+    }
+    
+    @RequestMapping(value = "/insegnante", method = RequestMethod.POST)
+    public String newInsegnante(@ModelAttribute("insegnante") Insegnante insegnante, 
+    									Model model, BindingResult bindingResult) {
+    	this.insegnanteValidator.validate(insegnante, bindingResult);
+        if (!bindingResult.hasErrors()) {
+        	this.insegnanteService.inserisci(insegnante);
+            model.addAttribute("insegnanti", this.insegnanteService.tutti());
+            return "insegnanti.html";
+        }
+        return "insegnanteForm.html";
+    }
 }
